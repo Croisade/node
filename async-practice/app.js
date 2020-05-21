237df80fe8a4889871ce8438264d28006ab8caa4
Jamal Gardiner <jgardiner@neat.com>
2020-05-21 11:01:17 -0400

    shows javascript running without asynchronous code

--- async-practice/app.js
+++ async-practice/app.js
@@ -1,8 +1,9 @@
-function otherFunct() {
-	console.log('we are in another funct');
-	console.log('do some stuff');
-}
+//Async code example
 
-console.log('Start')
-otherFunct();
-console.log('end')
+console.log('Start');
+
+setTimeout(() => {
+	console.log('we are in the timeout');
+}, 5000);
+
+console.log('end');
