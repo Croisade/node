7b05a15d3a2abdf8682df899319022ae9764732b
Jamal Gardiner <jgardiner@neat.com>
2020-05-21 11:58:09 -0400

    More callback functions

--- async-practice/app.js
+++ async-practice/app.js
@@ -2,8 +2,22 @@
 
 console.log('Start');
 
-setTimeout(() => {
-	console.log('we are in the timeout');
-}, 5000);
+function loginUser(email, password, callback) {
+	setTimeout(() => {
+		callback ({userEmail: email})
+	}, 1500);
+}
 
+function getUserVideos(email, callback){
+	setTimeout(() => {
+		callback (["video1", "video2", "video3"]);
+	}, 1000);
+}
+
+const user = loginUser('jamal@jamal.com', 123456, user => {
+	console.log(user);
+	getUserVideos(user.UserEmail, videos =>{
+		console.log(videos)
+	});
+});
 console.log('end');
