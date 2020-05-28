bd4cfee23b3a11742bfbbc4438af2025535c9559
Jamal Gardiner <jamalgardiner7@live.com>
2020-05-28 08:47:19 -0400

    Section 68 updates

--- 05-expressjs/app.js
+++ 05-expressjs/app.js
@@ -2,27 +2,15 @@ const express = require("express");
 const bodyParser = require('body-parser');
 const app = express();
 
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 app.use(bodyParser.urlencoded({extended: false}));
 
-app.use("/", (req, res, next) => {
-  console.log("This always runs");
-  next();
-});
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
 
-app.use("/add-product", (req, res, next) => {
-  console.log("In the middleware again!");
-	res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add product page</button></input></form>');
-});
-
-app.use("/product", (req, res, next) => {
-	console.log(req.body)
-	res.redirect('/')
-});
-
-
-app.use("/", (req, res, next) => {
-  console.log("Middleware is my middle name!");
-  res.send("<h1>Hello from express</h1>");
-});
+app.use((req, res) => {
+	res.status(404).send('<h1>Page not found </h1>')
+})
 
 app.listen(3000);
