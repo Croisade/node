fa41913093869548fdb5d34bde08496e2c94793e
Jamal Gardiner <jamalgardiner7@live.com>
2020-05-28 19:35:37 -0400

    Section 72 updates

--- 05-expressjs/routes/admin.js
+++ 05-expressjs/routes/admin.js
@@ -1,15 +1,16 @@
 const express = require('express')
-
 const router = express.Router();
+const path = require('path')
+
+const rootDir = require('../util/path')
 
 router.get("/add-product", (req, res, next) => {
-  	console.log("In the middleware again!");
-        res.send('<form action="/admin/add-product" method="POST"><input type="text" name="title"><button type="submit">Add product page</button></input></form>');
-});
+        res.sendFile(path.join(rootDir, 'views', 'add-product.html'))
+})
 
 router.post("/add-product", (req, res, next) => {
         console.log(req.body)
         res.redirect('/')
-});
+})
 
-module.exports = router;
+module.exports = router
